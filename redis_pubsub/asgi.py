b1be8014b33ca3faf6a7443bcddd1fd8c42812b6
Abdullah Al Rafi <abdullah34alrafi@gmail.com>
2023-03-23 03:05:08 +0600

    asgi config added

--- redis_pubsub/asgi.py
+++ redis_pubsub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for redis_pubsub project.
+ASGI config for portfolio_blog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import main.routing as rout
 import os
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'redis_pubsub.settings')
 
-application = get_asgi_application()
+asgi_application = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": asgi_application,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                rout.websocket_urlpatterns
+            )
+        )
+    ),
+})
